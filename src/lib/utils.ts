2437d3900dc7ba7bd3da21f573bcc561c69eced8
User <user@Users-MacBook-Pro.local>
2022-01-15 19:52:23 +0300

    dasda

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-const anon = import.meta.env.VITE_SUPABASE_ANON.toString()
-const url = import.meta.env.VITE_SUPABASE_URL.toString()
+const anon = import.meta.env.VITE_SUPABASE_ANON
+const url = import.meta.env.VITE_SUPABASE_URL
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(url, anon);
