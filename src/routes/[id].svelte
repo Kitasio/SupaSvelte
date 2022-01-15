638f0743afc315b0e5ce854ee07b37c9a3dac5a9
User <user@Users-MBP.netis>
2022-01-15 21:04:52 +0300

    redir

--- src/routes/[id].svelte
+++ src/routes/[id].svelte
@@ -2,23 +2,28 @@
 	import { supabase } from '$lib/utils';
 
 	export async function load({ params }) {
-		const { data, error } = await supabase.from('people').select().eq('id', params.id)
+		const { data, error } = await supabase.from('people').select().eq('id', params.id);
 
-		if (!error) {
+		try {
 			return {
 				props: {
 					person: data[0]
 				}
+			};
+		} catch {
+			return {
+				status: 301,
+				redirect: '/'
 			};
 		}
 	}
 </script>
 
 <script>
-    export let person;
+	export let person;
 </script>
 
 <div>
-    <h2>Name: {person.name}</h2>
-    <h3>Age: {person.age}</h3>
+	<h2>Name: {person.name}</h2>
+	<h3>Age: {person.age}</h3>
 </div>
