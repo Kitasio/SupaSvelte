0545e71012891d780d13a9f9c1ab39cb7a8292ee
User <user@Users-MacBook-Pro.local>
2022-01-15 20:03:49 +0300

    removed .env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-const anon = import.meta.env.VITE_SUPABASE_ANON
-const url = import.meta.env.VITE_SUPABASE_URL
+
+const url = 'https://wbndlzukwejbebtajlsi.supabase.co'
+const anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyMjU2NTMxLCJleHAiOjE5NTc4MzI1MzF9.hMA0VuTHlQjncIZNYrHEv2js5lpeiC5HARoUcbt1_fI'
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(url, anon);
